9748606851ef3ce058caf125f2ddf33f7471cd98
sleeping_sprite <li.zhongtao@hotmail.com>
2017-10-24 14:20:27 -0700

    Done
    new Launcher pure FTP anonymous user "DCW"
    fixed icon
    fixed shotcut

--- DCW_Launcher/Form_main.cs
+++ DCW_Launcher/Form_main.cs
@@ -30,31 +30,49 @@ namespace DCW_Launcher
             this.numericUpDown3.Value = DateTime.Now.Day;
 
             XmlDocument doc = new XmlDocument();
-            doc.Load("config.xml");
+            doc.Load("launcher_config.xml");
             if (doc != null)
             {
                 //分析文件  
                 XmlNode node = doc.SelectSingleNode("//DCW/configuration");
                 if (node != null)
                 {
-                    this.nowVersion = node.Attributes["version"].InnerText.Trim();
                     this.server = node.Attributes["server"].InnerText.Trim();
-                    this.path = node.Attributes["path"].InnerText.Trim();
-                    
-                    this.tb_nowVersion.Text = this.nowVersion;
                     this.tb_server.Text = this.server;
+                    this.path = node.Attributes["path"].InnerText.Trim();
                     this.tb_path.Text = this.path;
-                    this.tb_url.Text = this.server;
+                }
+                else
+                {
+                    MessageBox.Show("Local laucher_config.xml is defect", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Can't get local laucher_config.xml", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            string configURL = string.Format("{0}\\DCW\\config.xml", Application.StartupPath);
+            doc.Load(configURL);
+            if (System.IO.File.Exists(configURL))
+            {
+                //分析文件
+                XmlNode node = doc.SelectSingleNode("//DCW/configuration");
+                if (node != null)
+                {
+                    this.nowVersion = node.Attributes["version"].InnerText.Trim();
+                    this.tb_nowVersion.Text = this.nowVersion;
                     
                 }
                 else
                 {
                     MessageBox.Show("Local config.xml is defect", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
+
             }
             else
             {
-                MessageBox.Show("Can't get local config.xml", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                nowVersion= "0000.00.00.00";
+                this.tb_nowVersion.Text = nowVersion;
             }
 
 
@@ -64,11 +82,16 @@ namespace DCW_Launcher
         private void btn_check_Click(object sender, EventArgs e)
         {
             this.lastVersion = this.GetLastVersion(this.server);
-            this.tb_lastVersion.Text = this.lastVersion;            
-
-            if (string.IsNullOrEmpty(lastVersion) || string.IsNullOrEmpty(nowVersion))
+            if(string.IsNullOrEmpty(lastVersion))
             {
-                MessageBox.Show("Config.xml is defect", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Can't fetch last version from server", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            this.tb_lastVersion.Text = this.lastVersion;           
+            if (string.IsNullOrEmpty(nowVersion))
+            {
+                MessageBox.Show("Can't fetch version from local", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
             else
             {
@@ -81,7 +104,7 @@ namespace DCW_Launcher
                     {
                         //下载
                         this.lb_info.Items.Add("【1】Downloading the new software from server...");
-                        string filePath = this.Download(this.server);
+                        string filePath = this.Download(this.server,string.Format("DCW({0}).zip", this.lastVersion));
                         //更新
                         this.lb_info.Items.Add("【2】Installing the new software...");
                         this.UnZipFile(filePath);
@@ -114,7 +137,7 @@ namespace DCW_Launcher
                 if (node != null)
                 {
                     this.server = this.tb_server.Text.Trim();
-                    node.Attributes["server"].InnerText = this.server;
+                    node.Attributes["server"].InnerText = this.server;                    
                     doc.Save("config.xml");
                 }
                 else
@@ -131,31 +154,77 @@ namespace DCW_Launcher
         private string GetLastVersion(string server)
         {
             string version = null;
-            //获取服务器信息  
-            using (WebClient client = new WebClient())
+            Uri uri = new Uri(server);
+            string[] temp = this.GetFtpFileName(uri);
+            for(int i=0;i<temp.Length;i++)
             {
-                XmlDocument doc = new XmlDocument();
                 try
                 {
-                    doc.Load(client.OpenRead(server + "config.xml"));
-                    if (doc != null)
+                    string item = temp[i];
+                    if (item.Contains("DCW(") && item.Contains(").zip"))
                     {
-                        //分析文件  
-                        XmlNode node = doc.SelectSingleNode("//DCW/configuration");
-                        if (node != null)
+                        string verString = item.Substring(4, 13);
+                        int verTemp = Convert.ToInt32(verString.Replace(".", ""));
+                        if (string.IsNullOrEmpty(version))
+                            version = verString;
+                        else if (verTemp > Convert.ToInt32(version.Replace(".", "")))
                         {
-                            version = node.Attributes["version"].InnerText.Trim();
-
-                            //获取信息成功，退出函数
+                            version = verString;
                         }
                     }
                 }
-                catch
-                {
-                    MessageBox.Show("Can't connect to update server", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
+                catch { }
             }
+            
             return version;
+        }
+
+        /// <summary>
+        /// 直接传文件名获取信息
+        /// </summary>
+        /// <param name="userId">ftp userid</param>
+        /// <param name="pwd">ftp password</param>
+        /// <param name="ftpIP">ftp ip</param>
+        /// <returns></returns>
+        public string[] GetFtpFileName(Uri uri)
+        {
+            string[] downloadFiles;
+            StringBuilder result = new StringBuilder();
+            try
+            {
+                FtpWebRequest ftpRequest = null;
+                FtpWebResponse ftpResponse = null;
+                Stream ftpStream = null;
+                ftpRequest = (FtpWebRequest)FtpWebRequest.Create(uri);
+                ftpRequest.Credentials = new NetworkCredential("DCW", "");
+                ftpRequest.UseBinary = true;
+                ftpRequest.UsePassive = true;
+                ftpRequest.KeepAlive = true;
+                ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+                ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                ftpStream = ftpResponse.GetResponseStream();
+                StreamReader ftpReader = new StreamReader(ftpStream);
+                string line = ftpReader.ReadLine();
+                while (line != null)
+                {
+                    result.Append(line);
+                    result.Append("\n");
+                    line = ftpReader.ReadLine();
+                }
+                result.Remove(result.ToString().LastIndexOf('\n'), 1);
+
+                ftpReader.Close();
+                ftpStream.Close();
+                ftpResponse.Close();
+                ftpRequest = null;
+                return result.ToString().Split('\n');
+
+            }
+            catch (Exception ex)
+            {
+                downloadFiles = null;
+                return downloadFiles;
+            }
         }
 
         private string GetZipURL(string server)
@@ -188,25 +257,25 @@ namespace DCW_Launcher
             return url;
         }
 
-        private string Download(string server)
+        private string Download(string server,string fileName)
         {
-            string zipURL = this.GetZipURL(server);
+            //string zipURL = this.GetZipURL(server);
             if (!Directory.Exists(Application.StartupPath + "\\temp\\"))
             {
                 Directory.CreateDirectory(Application.StartupPath + "\\temp\\");
             }
 
-            string fileURL = Application.StartupPath + "\\temp\\" + this.lastVersion + ".zip";
+            string fileURL = string.Format("{0}\\temp\\DCW({1}).zip", Application.StartupPath, this.lastVersion); 
             FileStream fs = new FileStream(fileURL, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
 
-            HttpWebRequest objWebRequest = (HttpWebRequest)WebRequest.Create(zipURL);
-            objWebRequest.AllowAutoRedirect = true;
+            FtpWebRequest objWebRequest = (FtpWebRequest)WebRequest.Create(string.Format("{0}{1}",server,fileName));
+            //objWebRequest.AllowAutoRedirect = true;
             //              int nOffset = 0;  
             long nCount = 0;
             byte[] buffer = new byte[40960];   //40KB  
             int nRecv = 0;  //接收到的字节数  
 
-            HttpWebResponse objWebResponse = (HttpWebResponse)objWebRequest.GetResponse();
+            FtpWebResponse objWebResponse = (FtpWebResponse)objWebRequest.GetResponse();
             Stream recvStream = objWebResponse.GetResponseStream();
 
             /*
@@ -251,14 +320,16 @@ namespace DCW_Launcher
 
             using (ZipInputStream s = new ZipInputStream(System.IO.File.OpenRead(zipFilePath)))
             {
-
+                if (!Directory.Exists(Application.StartupPath + "\\DCW\\"))
+                {
+                    Directory.CreateDirectory(Application.StartupPath + "\\DCW\\");
+                }
                 ZipEntry theEntry;
                 while ((theEntry = s.GetNextEntry()) != null)
                 {
 
-                    Console.WriteLine(theEntry.Name);
-
-                    string directoryName = Path.GetDirectoryName(theEntry.Name);
+                    //Console.WriteLine(theEntry.Name);
+                    string directoryName = Path.GetDirectoryName(Application.StartupPath + "\\DCW\\"+theEntry.Name);
                     string fileName = Path.GetFileName(theEntry.Name);
 
                     // create directory
@@ -269,7 +340,7 @@ namespace DCW_Launcher
 
                     if (fileName != String.Empty)
                     {
-                        using (FileStream streamWriter = System.IO.File.Create(theEntry.Name))
+                        using (FileStream streamWriter = System.IO.File.Create(Application.StartupPath + "\\DCW\\" + fileName))
                         {
 
                             int size = 2048;
@@ -298,11 +369,11 @@ namespace DCW_Launcher
             IWshRuntimeLibrary.WshShell shell = new IWshRuntimeLibrary.WshShell();
 
             IWshRuntimeLibrary.IWshShortcut shortcut = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(DesktopPath + "\\DCW Datenbank.lnk");
-            shortcut.TargetPath =Application.StartupPath +"\\DCW.exe";
+            shortcut.TargetPath =Application.StartupPath +"\\DCW\\DCW.exe";
             shortcut.Arguments = "";// 参数
             shortcut.Description = "DCW Datenbank";
-            shortcut.WorkingDirectory = Application.StartupPath;//程序所在文件夹，在快捷方式图标点击右键可以看到此属性
-            shortcut.IconLocation =Application.StartupPath +"DCW.exe,0";//图标
+            shortcut.WorkingDirectory = Application.StartupPath+"\\DCW";//程序所在文件夹，在快捷方式图标点击右键可以看到此属性
+            shortcut.IconLocation =Application.StartupPath + "\\DCW\\DCW.exe,0";//图标
             //shortcut.Hotkey = "CTRL+SHIFT+Z";//热键
             shortcut.WindowStyle = 1;
             shortcut.Save();
@@ -316,13 +387,13 @@ namespace DCW_Launcher
             {
                 this.path = this.folderBrowserDialog.SelectedPath;
                 this.tb_path.Text = path;
-                XmlDocument doc = new XmlDocument();
-                doc.Load("config.xml");
-                XmlNode xmlNode = doc.SelectSingleNode("//DCW/configuration");
+                //XmlDocument doc = new XmlDocument();
+                //doc.Load("config.xml");
+                //XmlNode xmlNode = doc.SelectSingleNode("//DCW/configuration");
 
-                xmlNode.Attributes["path"].InnerText = this.path;
+                //xmlNode.Attributes["path"].InnerText = this.path;
                 
-                doc.Save("config.xml");
+                //doc.Save("config.xml");
                 
             }
         }
@@ -331,28 +402,29 @@ namespace DCW_Launcher
         {
             string version = Convert.ToUInt16(this.numericUpDown1.Value).ToString("d4") + "." + Convert.ToUInt16(this.numericUpDown2.Value).ToString("d2") + "." + Convert.ToUInt16(this.numericUpDown3.Value).ToString("d2") + "." + Convert.ToUInt16(this.numericUpDown4.Value).ToString("d2");
             string filename = "DCW(" + version + ").zip";
-            string URL = this.tb_url.Text.Trim() + filename;
+            //string URL = this.tb_url.Text.Trim() + filename;
 
             //生成升级包之前，将界面上的参数存储到Launcher的xml文件
-            XmlDocument doc = new XmlDocument();
-            doc.Load("config.xml");
-            XmlNode xmlNode = doc.SelectSingleNode("//DCW/configuration");
-            xmlNode.Attributes["server"].InnerText = this.tb_server.Text.Trim();
-            xmlNode.Attributes["path"].InnerText = this.tb_path.Text.Trim();
-            xmlNode.Attributes["version"].InnerText = version;
-            xmlNode.Attributes["url"].InnerText = URL;
-            doc.Save("config.xml");
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load("config.xml");
+            //XmlNode xmlNode = doc.SelectSingleNode("//DCW/configuration");
+            //xmlNode.Attributes["server"].InnerText = this.tb_server.Text.Trim();
+            //xmlNode.Attributes["path"].InnerText = this.tb_path.Text.Trim();
+            //xmlNode.Attributes["version"].InnerText = version;
+            //xmlNode.Attributes["url"].InnerText = URL;
+            //doc.Save("config.xml");
             //生成升级包之前，将界面上的参数存储到DCW的xml文件
             XmlDocument doc1 = new XmlDocument();
-            doc1.Load(this.tb_path.Text.Trim()+"\\config.xml");
+            doc1.Load(this.tb_path.Text.Trim() + "\\config.xml");
             XmlNode xmlNode1 = doc1.SelectSingleNode("//DCW/configuration");
-            xmlNode1.Attributes["server"].InnerText = this.tb_server.Text.Trim();
-            xmlNode1.Attributes["path"].InnerText = this.tb_path.Text.Trim();
-            xmlNode1.Attributes["version"].InnerText = version;
-            xmlNode1.Attributes["url"].InnerText = URL;
+            //xmlNode1.Attributes["server"].InnerText = this.tb_server.Text.Trim();
+            
+            xmlNode1.Attributes["version"].InnerText = version;//修改DCW程序中的版本信息
+            //xmlNode1.Attributes["version"].InnerText = version;
+            //xmlNode1.Attributes["url"].InnerText = URL;
             doc1.Save(this.tb_path.Text.Trim() + "\\config.xml");
 
-            
+
             if (!string.IsNullOrEmpty(this.path))
             {
                 //string targetPath = Path.Combine(this.folderBrowserDialog.SelectedPath, "testtt.zip");
@@ -381,11 +453,11 @@ namespace DCW_Launcher
                     //压缩升级文件到指定位置
                     int x = this.Zip(Path.Combine(copyTargetPath, filename), fileList.ToArray(), "Document", null);
                     //复制config.xml到指定位置
-                    string copySourcePath = System.Windows.Forms.Application.StartupPath;
-                    string ConFileName = "config.xml";
-                    string sourceFile = System.IO.Path.Combine(copySourcePath, ConFileName);
-                    string destFile = System.IO.Path.Combine(copyTargetPath, ConFileName);
-                    System.IO.File.Copy(sourceFile, destFile, true);
+                    //string copySourcePath = System.Windows.Forms.Application.StartupPath;
+                    //string ConFileName = "launcher_config.xml";
+                    //string sourceFile = System.IO.Path.Combine(copySourcePath, ConFileName);
+                    //string destFile = System.IO.Path.Combine(copyTargetPath, ConFileName);
+                    //System.IO.File.Copy(sourceFile, destFile, true);
                 }
                 
             }
@@ -560,11 +632,28 @@ namespace DCW_Launcher
 
         private void btn_run_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("DCW.exe", "");
-            this.Close();
+            System.Diagnostics.Process.Start(Application.StartupPath + "\\DCW\\DCW.exe", "");
+            //System.Diagnostics.Process.Start(@"C:\Users\Administrator\Documents\GitHub\DCW\DCW_Launcher\bin\Debug\DCW\DCW.exe");
+
+            
+            //this.Close();
         }
 
-
+        private void tb_path_TextChanged(object sender, EventArgs e)
+        {
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load(this.tb_path.Text.Trim() + "\\config.xml");
+            XmlNode node = doc1.SelectSingleNode("//DCW/configuration");
+            if (node != null)
+            {
+                string version = node.Attributes["version"].InnerText.Trim();
+                string[] ver=version.Split('.');
+                numericUpDown1.Value = Convert.ToDecimal(ver[0]);
+                numericUpDown2.Value = Convert.ToDecimal(ver[1]);
+                numericUpDown3.Value = Convert.ToDecimal(ver[2]);
+                numericUpDown4.Value = Convert.ToDecimal(ver[3]);
+            }
+        }
     }
 
 }
